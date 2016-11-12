d3a4b9198de3d0c84b30893ad087bf4c6755acb0
Udit Desai <desaiuditd@gmail.com>
2016-11-12 11:30:13 -0800

    updates DB schema info

--- imagely/settings.py
+++ imagely/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'imagely.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'imagely',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
